391ad9543d5217196f947014efd1d854f2fb53e1
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-10 13:22:23 +0100

    Styling home page and the whole site

--- reactapp/src/Pages/Home.jsx
+++ reactapp/src/Pages/Home.jsx
@@ -8,12 +8,12 @@ export default function Home(){
     const navigate = useNavigate();
 
     return(
-        <div className="flex flex-col justify-center text-center text-[#3285d7] overscroll-none">
-        <h1 className="text-6xl">Whisp</h1>
-        <Stack spacing={2} className="flex flex-col items-center justify-center h-screen">
-            <Button onClick={() => navigate("/login")} variant="outlined">Login</Button>
-            <Button onClick={() => navigate("/register")} variant="outlined">Register</Button>
-        </Stack>
+        <div className="flex flex-col justify-center text-center text-[#3285d7]">
+            <h1 className="text-6xl mt-10">Whisp</h1>
+            <Stack spacing={2} className="flex flex-col items-center justify-center h-screen">
+                <Button onClick={() => navigate("/login")} variant="outlined" size="large">Login</Button>
+                <Button onClick={() => navigate("/register")} variant="outlined" size="large">Register</Button>
+            </Stack>
         </div>
     )
 }
