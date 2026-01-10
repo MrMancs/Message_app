2d941a9491295e9d588738312968ec0c59362981
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-10 12:17:28 +0100

    Add routing and create Login and Register pages

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import {Routes, Route, useNavigate} from "react-router-dom";
+
+import Button from '@mui/material/Button';
+
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 export default function App(){
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>React</h1>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Button onClick={() => navigate("/login")} className="flex">Login</Button>
+      <Button onClick={() => navigate("/register")} className="flex">Register</Button>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   )
 }
