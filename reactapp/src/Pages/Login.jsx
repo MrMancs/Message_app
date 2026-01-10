7fa95e5337ca584a69e19389aa3343dbf7837ca4
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-10 14:40:54 +0100

    Implement Login page with form fields and navigation

--- reactapp/src/Pages/Login.jsx
+++ reactapp/src/Pages/Login.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Login(){
-    return (
-        <div>
-            <h1>Login Page</h1>
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-linear-to-r from-red-200 to-orange-200">
+      <Stack
+        spacing={2}
+        className="flex flex-col items-center justify-center h-screen"
+      >
+        <div className="flex flex-col items-center justify-center gap-4 h-[450px] w-[350px]">
+          <h1 className="text-2xl pb-2 text-[#3285d7] font-bold">Login</h1>
+
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            required
+            className="w-[250px]"
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            required
+            type="password"
+            className="w-[250px]"
+          />
+          <div className="flex flex-row justify-center gap-5">
+            <Button onClick={() => navigate("/chat")} variant="outlined">
+              Login
+            </Button>
+            <Button onClick={() => navigate("/")} variant="outlined">
+              Back
+            </Button>
+          </div>
         </div>
-    )
+      </Stack>
+    </div>
+  );
 }
