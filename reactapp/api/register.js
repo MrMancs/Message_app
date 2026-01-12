ab91f2e2dafcbe0c6fbc86930002faff974d3016
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-12 19:47:03 +0100

    Attempt fix INSERT INTO #2

--- reactapp/api/register.js
+++ reactapp/api/register.js
@@ -4,7 +4,7 @@ export default async function Register(req, res) {
     var pool = new Pool({
         connectionString: process.env.DATABASE_URL
     })
-    console.log(req.body)
+    console.log(req.body.username, req.body.email, req.body.password)
 
     pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", [req.body.username, req.body.email, req.body.password])
 
