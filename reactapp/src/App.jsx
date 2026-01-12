3700db4f09e6b2ca77cb3d2f77e6c7cdc86b17e3
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-12 08:37:58 +0100

    Add Chat route to App component

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
+import Chat from "./Pages/Chat";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/chat" element={<Chat/>} />
     </Routes>
   );
 }
