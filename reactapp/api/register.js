5551dfe01196eb154374d602d87e0d2cefd09741
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-12 19:59:55 +0100

    Attempt fix INSERT INTO #5

--- reactapp/api/register.js
+++ reactapp/api/register.js
@@ -1,14 +1,17 @@
 import { Pool } from "@neondatabase/serverless";
 
 export default async function Register(req, res) {
-    var pool = new Pool({
-        connectionString: process.env.DATABASE_URL
-    })
-    
-    const data = await req.json();
+  var pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
 
-    pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", [data.username, data.email, data.password])
+  const { username, email, password } = req.body;
 
-    const result = await pool.query("SELECT * FROM users")
-    console.log(result)
+  pool.query(
+    "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+    [username, email, password]
+  );
+
+  const result = await pool.query("SELECT * FROM users");
+  console.log(result);
 }
