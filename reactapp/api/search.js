f6e6cf94b1dd02b5283fdaad5b4745a4a14f4a92
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-13 16:07:21 +0100

    Refactor response handling in search function to remove status codes for user validation

--- reactapp/api/search.js
+++ reactapp/api/search.js
@@ -13,9 +13,9 @@ export default async function Search(req, res) {
     ]);
 
     if (result.rows.length === 0) {
-      return res.status(401).json({ message: "Invalid user" });
+      return res.json({ message: "Invalid user" });
     } else {
-      return res.status(200).json({ users: result.rows });
+      return res.json({ users: result.rows });
     }
   } catch (error) {
     return res.status(500).json({ message: "Error while searching" });
