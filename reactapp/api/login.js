24238a29b37d64998afa911e8c92cad932f70a52
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-13 11:44:01 +0100

    Attempt fix login #2

--- reactapp/api/login.js
+++ reactapp/api/login.js
@@ -13,7 +13,7 @@ export default async function Login(req, res) {
       password,
     ]);
 
-    console.log(result)
+    console.log(result.rows)
 
     return res.status(200).json({ message: "Login Successful!" });
   } catch (error) {
