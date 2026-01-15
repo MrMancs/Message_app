dfc31b27b23f313722f99b449e624242202be6ac
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-15 20:45:55 +0100

    Add friends function to query friendships where status is accepted

---
+++ reactapp/api/friends.js
@@ -0,0 +1,22 @@
+import { Pool } from "@neondatabase/serverless";
+
+export default async function Friends(req, res) {
+  var pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  try {
+    const { requesterName, receiverName } = req.body;
+
+    const result = await pool.query(
+      "SELECT * FROM friendships WHERE receiver_name = $1 AND requester_name = $2 AND status = 'accepted'",
+      [receiverName, requesterName]
+    );
+
+    console.log(result)
+
+    return res.status(200).json({ friends: "result" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error while fetching friends" });
+  }
+}
