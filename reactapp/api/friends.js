48ca004679d816d1eefd0fd8ff268ccf5cde9df9
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-15 21:14:02 +0100

    Modify logs

--- reactapp/api/friends.js
+++ reactapp/api/friends.js
@@ -8,14 +8,10 @@ export default async function Friends(req, res) {
   try {
     const { requesterName, receiverName } = req.body;
 
-    console.log(requesterName, receiverName);
-
     const result = await pool.query(
       "SELECT * FROM friendships WHERE receiver_name = $1 AND requester_name = $2 AND status = 'accepted'",
       [receiverName, requesterName]
     );
-
-    console.log(result)
 
     return res.status(200).json({ friends: result.rows });
   } catch (error) { 
