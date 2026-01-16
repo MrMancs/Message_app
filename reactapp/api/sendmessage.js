c36f39057b02782d3de172a094587b3d32630853
MrMancs <marcell.szatmari2007@gmail.com>
2026-01-16 18:26:50 +0100

    Remove" console.log"-s

--- reactapp/api/sendmessage.js
+++ reactapp/api/sendmessage.js
@@ -7,7 +7,6 @@ export default async function sendMessage(req, res) {
 
   try {
     const { sender, receiver, message, sentAt } = req.body;
-    console.log("Küldéshez szükséges adatok:", sender, receiver, message, sentAt);
 
     const result = await pool.query(
       "INSERT INTO messages (sender, receiver, message, created_at) VALUES ($1, $2, $3, $4)",
@@ -18,7 +17,6 @@ export default async function sendMessage(req, res) {
 
     return res.status(200).json({ message: "Message sent!", status: 200 });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ message: "Error while sending message" });
   }
 }
